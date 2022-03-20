2add83781f633f4a434c77b4a5be5503a45acb61
FlavioValladolid <flavioisay@hotmail.com>
2022-03-20 06:37:11 -0700

    Updating elements

--- 04 - JS/public/js/pokedex.js
+++ 04 - JS/public/js/pokedex.js
@@ -1,19 +1,34 @@
 const fetchPokemon = () => {
-    let pokeInput = pokeName.value;
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value; 
+    pokeName = pokeName.toLowerCase(); 
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
     fetch(url).then((res) => {
-        return res.json();
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("./public/img/pikachuSleeping.png");
+        } 
+        else {
+            return res.json();
+        }
+
     }).then((data) => {
         console.log(data);
-        let pokeImg = data.sprites.font_default;
-        console.log(pokeImg)
-        pokeImg(pokeImg)
+        let pokeImg = data.sprites.front_default;
+        pokeImage(pokeImg);
+        console.log(pokeImg);   
+            // Nombre
+            // tipo de pokemon
+            // estadisticas
+            // movimientos
     });
-};
-
-// fetchPokemon();
-
-const pokeImage = (url) => {
-    const pokeImg = document.getElementById("pokeImg");
-    pokeImg.src = url;
 }
+const pokeImage = (url) => {
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
+
+// const pokeNameF = (url) => {
+//     const pokeNameOutput = document.getElementById("pokeNames");
+//     pokeNameOutput.innerHTML = "NewText";
+// }
