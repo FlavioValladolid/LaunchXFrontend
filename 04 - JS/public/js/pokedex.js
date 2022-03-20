61f5b78013ca9feef9f21975983bafd478dda3eb
FlavioValladolid <flavioisay@hotmail.com>
2022-03-20 08:12:30 -0700

    update index.html and pokedex.js with pokemon stats

--- 04 - JS/public/js/pokedex.js
+++ 04 - JS/public/js/pokedex.js
@@ -20,6 +20,16 @@ const fetchPokemon = () => {
         console.log(pokeImg);  
         pokeNameF(pokeName); 
         pokeType(data["types"][0].type["name"]);
+        for (let statElement = 0; statElement < data["stats"].length; statElement++) {
+            // Stat name
+            console.log("Stat Name: " + data["stats"][statElement].stat["name"])
+            // Base stat
+            console.log("Base Stat: "+data["stats"][statElement].base_stat)
+            // effort stat
+            console.log("Effort: "+data["stats"][statElement].effort)
+            // const element = array[i];
+            
+        }
 
             // estadisticas
             // movimientos
@@ -38,4 +48,8 @@ const pokeNameF = (url) => {
 const pokeType = (url) => {
     const pokeTypeOutput = document.getElementById("pokeTypes");
     pokeTypeOutput.innerHTML = url;
+}
+const pokeStat = (url) => {
+    const pokeStatOutput = document.getElementById("pokeStats");
+    pokeStatOutput.innerHTML = url;
 }
